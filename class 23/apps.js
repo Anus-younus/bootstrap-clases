a3bd68bdee365fe27388e4f44dd47ce335fe2ef3
Anus Younus <AnusYounus254@gmail.com>
2024-02-04 23:01:57 +0500

    save

---
+++ class 23/apps.js
@@ -0,0 +1,88 @@
+// Method 
+// alert('Anus')
+
+// console.log(12345)
+
+// var Name = 'Anus'
+
+// alert(Name)
+
+var firstname = 'Muhammad'
+
+var lastname = 'Anus'
+
+var name = 'Younus'
+
+
+console.log('Name' ,firstname)
+
+console.log('First Name',lastname)
+
+console.log('Last Name',name)
+
+var weight = 160
+
+console.log('1' ,weight)
+
+var weight = 150
+
+console.log('2' ,weight)
+
+
+// Camelcase
+ var firstName = ('Class')
+
+ alert(firstName)
+
+ var whatsLeftOver = 9 % 3;
+
+ console.log(whatsLeftOver)
+
+//  document.write('<h1> Ghous <br/> Ahmed </h1>')
+
+
+var abc;
+
+console.log(abc)
+
+abc ='Hello'
+
+console.log(abc)
+
+var a = 'Hello' , b = 'World'
+
+console.log(a,b)
+
+var totalCost = 1 + (3 * 4);
+
+console.log(totalCost)
+
+var TotalCost = (1 + 3) * 4;
+
+console.log(TotalCost)
+
+
+var total = (2 + 3) * 4
+
+console.log(total)
+
+var a = 4;
+var b = 3;
+
+var c = a++ - b-- + --a + a++;
+
+console.log(c)
+
+var c = --b + --a - ++b  + a++ - --a;
+
+console.log(c)
+
+var num = '2' + 2;
+
+console.log(typeof num)
+
+var A = '3' + '3'
+
+console.log(A)
+
+typeof A;
---
+++ class 23/enter.js
@@ -0,0 +1,21 @@
+var firstName = 'Anus'
+
+var lastName = 'Bandija'
+
+
+var fullName = firstName + ' ' + lastName + '!'
+
+console.log(fullName)
+
+document.write("<h1>"  + fullName +  "</h1>")
+
+
+var Firstname = prompt('Enter Your First Name', 'Ghous')
+
+var Lastname = prompt('Enter Your Last Name')
+
+
+var Fullname = Firstname + ' ' + Lastname + '!'
+
+console.log(Fullname)
+
