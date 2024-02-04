247b9696decc064c5ad307f6e541a0254df4412a
Anus Younus <AnusYounus254@gmail.com>
2024-02-04 13:18:32 +0500

    save'

---
+++ class 22/app.js
@@ -0,0 +1,66 @@
+// alert("HEY")
+
+// 232323
+// true
+
+"hello"
+
+// // alert()
+// window.alert("anus")
+
+// var fullname = "Anus Younus";
+
+// fullname = "Zain"
+// alert("Owais")
+
+// alert("Owais")
+
+// alert("Owais")
+
+// alert("Owais")
+// alert(fullname)
+
+
+// var name = 'Anus'
+
+// alert(name)
+
+
+// myFull
+// my-name
+// my_full_name
+
+
+// var num =20;
+
+
+// alert(num)
+
+
+// num = num + 10;
+
+// alert(num)
+
+// var num = 20;
+
+// num = num + 10;
+
+// alert(num)
+
+
+// var fullname = "ghous ahmed"
+
+
+// alert(fullname + 10 + 5)
+
+// var num = 'Anus'
+
+// alert(num)
+
+
+// console.log('anus');
+
+
+var num = 10;
+
+console.log(num % 10)
