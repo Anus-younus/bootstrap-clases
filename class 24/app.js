0dd3e07b15c0f2c500026564509b6c90750556af
Anus Younus <AnusYounus254@gmail.com>
2024-02-06 11:26:45 +0500

    save

---
+++ class 24/app.js
@@ -0,0 +1,76 @@
+// console.log('Hello World')
+
+// var totalcost = 1 + 3 * 4
+
+// console.log('totalcost===>', totalcost)
+
+// var message = 'Hello World'
+
+
+// console.log(typeof message)
+
+// var num = 23
+
+// console.log(typeof num)
+
+// var username = 'Anus'
+
+// console.log('Enter Your Name' + username + 'thanks')
+
+// var firstName = 'Anus'
+
+// var lastName = 'Younus'
+
+// console.log(firstName + ' ' + lastName)
+
+// var firstName = prompt('Enter Your First Name' , 'Anus')
+
+// var lastName = prompt('Enter Your Last Name' , 'Younus')
+
+// var fullName = firstName + '' + lastName + ' '
+
+// console.log(fullName)
+
+// var question = 'Enter Your Gender'
+// var correctAnsware = 'male'
+// var answare = prompt()
+
+// console.log(answare = prompt(question,correctAnsware))
+
+var x = prompt("Where does the Pope live?");
+if (x === "Vatican") {
+    alert("Correct!");
+}
+if(true){
+ alert('Hello')
+}
+var gender = prompt('Enter Your Gender', 'Male')
+
+var message = 'Not Match'
+
+// if(gender==='Male'){
+//     alert('Hello Sir')
+// }
+// if(gender==='Female'){
+//     alert('Hello Sister')
+// }
+
+if(gender == 'Male'){
+    message = 'Hello Sir'
+}
+
+if(gender == 'Female'){
+    message = 'Hello Sister'
+}
+ alert(message)
+
+//  '2' == '2'
+
+//  2 === '2'
+
+//  2 == '2'
+
+var education =prompt('Enter You Education')
+if(education !== 'matric'){
+    alert('Eligible')
+}
---
+++ class 24/html.js
@@ -0,0 +1,45 @@
+// var age = prompt('Enter Your age')
+
+// if(age >= 20){
+//     alert('Eligible')
+//     }
+// else if(age === 'ok'){
+//     alert('ok')
+// }
+// else{
+//     alert('Not Eligible')
+// }
+
+// var lanakyahai = prompt('kya mil raha he')
+// var kg = 1;
+
+// if(lanakyahai === 'dahi' && kg == 1){
+//     alert('tw le ana')
+// }
+// else if (lanakyahai === 'doodh'){
+//     alert('Cahlo Le ana')
+// }
+// else if(lanakyahai === 'bread'){
+//     alert('abe yar le ana')
+// }
+// else{
+//     alert('Kuch be le ana')
+// }
+
+var language = prompt("Enter Your Language");
+
+if (language === "javascript") {
+  var city = prompt("Enter Your City");
+  if (city === "karachi") {
+    var exp = prompt("Enter your experince");
+    if (exp === "2 years") {
+      alert("cogragulation");
+    } else {
+      alert("bare ho kar ao");
+    }
+  } else {
+    alert("karachi jao");
+  }
+} else {
+  alert("javascript sekho");
+}
