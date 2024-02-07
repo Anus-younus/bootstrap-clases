d23ed3fe12f5b5efa433ef7fb56af287bc5d329c
Anus Younus <AnusYounus254@gmail.com>
2024-02-08 02:18:00 +0500

    save

---
+++ class 25/apps.js
@@ -0,0 +1,29 @@
+var userInput = prompt('Where is saylani', 'shoe me')
+if(userInput.toLowerCase() === 'karachi'){
+    console.log('correct')
+}else{
+    console.log('wrong answare')
+}
+var myansware = prompt('karachi')
+if(myansware === 'karachi'){
+    console.log('coreect')
+}
+
+// !=   > greter than < less than
+
+var myAnsware = 10;
+if(myAnsware === 10){
+    consol.log('correct')
+}
+else if(myAnsware === 12 ){
+    consol.log('this is correct')
+}
+else{
+    consol.log('wrong')
+}
+
+var str = '10'
+
+console.log('Type===> str')
+
+console.log(typeof str)
---
+++ class 25/next.js
@@ -0,0 +1,18 @@
+// var userName = prompt('Engter your user name')
+// if(userName === ""){
+//     alert('Not provided')
+// }else{
+//     alert(userName)
+// }
+// var userName = prompt('Enter your username' || 'Not provided')
+// console.log(userName);
+// !!""
+// !!0
+// !!false
+// undefined
+// null
+// NaN
+var userName = prompt("Enter your username");
+var isAvalible = userName && "Yes"
+console.log(isAvalible);
+console.log(userName);
