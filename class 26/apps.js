8d8dae39eb31a19640a261bc847220c677ef6f3d
Anus Younus <AnusYounus254@gmail.com>
2024-02-09 20:40:09 +0500

    save changes

---
+++ class 26/apps.js
@@ -0,0 +1,75 @@
+// var student1 = 'Anus'
+// var student2 = 'Owais'
+
+// var students = ['Raza','Manan','Rafay']
+
+// students[3] = 'Ali'
+// students[4] = 'lodo'
+
+// var totaLength = students.length;
+
+// students[students.length -1] = 'Xyz'
+
+// console.log(students.length)
+
+// var students = [];
+
+// students[students.length] = 'Anus'
+// students[students.length] = 'Younus'
+
+// console.log(students);
+
+// var students = ['Raza','Manan','Rafay']
+
+// var returnValue = students.shift();
+// var returnValue = students.pop();
+
+// var returnValue = students.push('ABC', 'XYZ', 'foo');
+// var returnValue = students.unshift('ABC', 'XYZ', 'foo');
+
+// console.log(returnValue)
+// var students = ['Raza','Manan','Rafay'];
+
+// students.splice(0,1);
+// students.splice(0,0, 'Anus', 'Owais', 'Manlo');
+// students.splice(1, 0, "ABC")
+// students.splice(0, 1, "ABC")
+
+// var returnValue = students.splice(0,0,"Anus");
+
+// console.log(returnValue)
+//  console.log(students);
+
+// var students = ['Anus', 'Younus']
+
+// students[5] = 'ABC'
+
+// console.log(students)
+
+
+
+// var userName = prompt('Enter your name')
+// if (userName === ""){
+//     alert('empty')
+// }else{
+//     alert(userName + 'Welcome')
+// }
+
+var students = ['Raza','Manan','Rafay'];
+
+
+students.slice(1,3)
+
+// var returnValue = students.slice(1,4);
+var returnValue = students.slice(-3);
+
+console.log(returnValue)
+
+console.log(students)
+
+var str = 'anus younus'
+
+// var copy = str.slice(-7)
+var copy = str.slice(0,5)
+
+console.log(copy)
