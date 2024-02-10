417ad403a9276a558a314a039a8917217b594eaf
Anus Younus <AnusYounus254@gmail.com>
2024-02-10 23:07:08 +0500

    save changes

---
+++ class 27/apps.js
@@ -0,0 +1,36 @@
+// var students = ['Anus', 'Owais', 'Raza']
+// var value1 = students.pop()
+// var value2 = students.push('Raza')
+// var value3 = students.shift()
+// var value4 = students.unshift('Anus')
+// console.log(value1)
+// console.log(value2)
+// console.log(value3)
+// console.log(value4)
+// console.log(students [2])
+// console.log(students)
+// var cites = ['karachi', 'islamabad', 'lahore', 'islamabad', 'peshawar']
+// var newData1 = cites.slice(1, 3)
+// var newData2 = cites.splice(0, 2, 'mary', 'skurdu')
+// console.log(newData1)
+// console.log(newData2)
+// console.log(cites)
+
+
+//for(statement, condition, increment){
+    // block
+//}
+
+
+
+//var text = 'hello world'
+
+// for(var i = 1; i <=5; i++){
+//     console.log('iterastion===>', i, text)
+// }
+
+var cites = ['karachi', 'islamabad', 'lahore', 'multan', 'peshawar'];
+
+for(var i = 0; i<=cites.length -1; i++){
+    console.log('cities===>', i, cites[i])
+}
