884d49ae8af7719aeda0ecd28ae850d7adeabafb
Anus Younus <AnusYounus254@gmail.com>
2024-02-12 10:52:47 +0500

    saave changes

---
+++ class 28/apps.js
@@ -0,0 +1,34 @@
+// console.log('test')
+
+// for (var i = 0; i <= 5; i++){
+//   console.log('test')
+//  }
+
+// var arr = ['Anus', 'Owais', 'Raza', 'Mnan']
+//  for (var i = 0; i < arr.length; i++){
+//   console.log(i,)
+//  }
+
+//  var arr = ['Anus', 'Owais', 'Raza', 'Mnan']
+// var flag = 'no'
+//  for(var i = 0; i < arr.length; i++){
+//  if(arr === 'Anus'){
+//   flag = 'yes'
+//   alert('mil gaya')
+//   break
+//  }
+//  }
+
+//  if(flag === 'no'){
+//   alert('nahi mil gaya')
+//  }
+
+// for(var i = 0; i < 10; i++){
+//     console.log(i);
+//     if(i === 0){
+//     break
+//     }
+// }
+
+
+
