42fcf05e56c4d449c30951065d6010a6a4d78a5c
Anus Younus <AnusYounus254@gmail.com>
2024-02-20 21:30:46 +0500

    save

---
+++ class 29/apps.js
@@ -0,0 +1,93 @@
+// minimum number or maximum number
+// var arr = [2,3,4,5,6,7]
+// var min = arr[0];
+// for(var i = 0; i < arr.length; i++ ){
+// console.log(arr[i])
+// if (arr [i] < min){
+//     if (arr [i] > min){
+//         min = arr [i];
+
+//     }
+// }
+
+// console.log(min)
+
+// var arr = ['ghous', 'ahmad', 'Zain'];
+
+// zain ahmad ghous
+// for (var i = 0; i < 5; i++){
+//     for(var j = 0; j < 5; j++){
+//         console.log(j, i);
+//     }
+// }
+
+// var arr = [['Ghous', 'ahmad'],['zain','Khan']]
+
+// for (var i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+//
+
+// var arr = [['Ghous', 'ahmad'],['zain','Khan']]
+
+// for (var i = 0; i < arr.length; i++){
+//     for(var j = 0; j < arr[i].length; j++)
+//     console.log(arr[i][j])
+// }
+
+// var arr = [['Ghous']]
+
+// arr[0]
+
+//
+
+// var full = 'Ghous Ahmad'
+
+// console.log(full.toUpperCase())
+
+// "string"
+
+// "string".slice(0,2)
+
+// var fullName = "ghous"
+// var firstChar = fullName.slice(0,1).toUpperCase();
+// var result = firstChar + fullName.slice(1);
+// console.log(result)
+
+// var text = "My Name is Ghous Ahmad";
+// for (var i = 0; i < text.length; i++){
+//     console.log(text.slice(i, i + 5) === "Ghous");
+// }
+
+// var text = "CAF America — We streamline the grantmaking process to eliminate risk and administrative burden. Advancing progress in society towards a fair and sustainable future for all. Industry-Leading."
+// var user = prompt("Enter your words")
+
+// for (var i = 0; i < text.length; i++){
+//     if(text.slice(i, i + user.length).toLocaleLowerCase === user.toLocaleLowerCase){
+//         console.log("found")
+//     }else{
+//         console.log("Not Found")
+//     }
+// }
+
+// var text = "CAF America — We streamline the grantmaking process to eliminate risk and administrative burden. Advancing progress in society towards a fair and sustainable future for all. Industry-Leading."
+
+// console.log(text.indexOf("anud"))
+
+// if (text.indexOf("CAF")!== -1){
+//    console.log("found")
+// }else{
+//     console.log("Not Found")
+// }
+
+// var text = "CAF America — We streamline the grantmaking process to eliminate risk and administrative burden. Advancing progress in society towards a fair and sustainable future for all. Industry-Leading."
+
+// console.log(text.indexOf("CAF"));
+// console.log(text.charAt(4));
+
+
+
+var fullName = "anus"
+
+console.log(fullName.toUpperCase())
