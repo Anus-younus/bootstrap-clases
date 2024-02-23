79954079ed9bcf5ac170972a6b53c49162a066e1
Anus Younus <AnusYounus254@gmail.com>
2024-02-23 22:36:29 +0500

    new javascript class

---
+++ class 30/apps.js
@@ -0,0 +1,40 @@
+// var arr =[['Ghous', 'Anus'],['Owais', 'Younus']];
+// for (var i = 0; i < arr.length; i++){
+//     for (var j = 0; j < arr[i].length; j++){
+//         console.log(arr[i][j])
+//     }
+// // }
+// var str = "saylani Bhadurabad";
+// console.log(str.replace('saylani', 'Anus'));
+
+// var string = "Anus Anus";
+
+// console.log(string.replaceAll('Anus', 'Younus'));
+
+// var random = Math.round(Math.random()*10);
+// console.log(random);
+
+// 200" - "duck"
+// NaN
+// +"200"  
+// 200
+// parseInt"290766"
+
+var num = 232345355
+
+console.log(num.toString());
+
+console.log(num.toFixed(3))
+var text = "My name is anus"
+
+var user = prompt("Enter your words");
+
+for (var i = 0; i < text.length; i++){
+    if(text.slice(i, i + user.length)!== user.length){
+        console.log("found")
+    }else{
+        console.log("not found")
+    }
+}
+
+
