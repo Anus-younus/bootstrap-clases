35d20d561ee77f9ad5902a2f9b9edce978e09ba1
Anus Younus <AnusYounus254@gmail.com>
2024-02-25 13:53:34 +0500

    save javascript-class-31

--- class 31/apps.js
+++ class 31/apps.js
@@ -1,2 +1,21 @@
 var students = ['Anus', 'Qwaus', 'Raza', 'Hassan']
+var result = students.push("Khan")
+var  result = students.pop()
+var  result = students.shift()
+var  result = students.unshift("Khan")
+var  result = students.slice(0,1)
+var  result = students.splice(0,1)
+// var  result = students.splite("string")
+var  result = students.indexOf()
 console.log(students)
+
+var text = "My name is anus younus"
+var user = prompt("Enter your words")
+for(var i = 0; i < text.length; i++){
+    if(text.slice(i, i + text.length)!== user.length){
+        console.log("found")
+    }
+}
+if(true){
+
+}
