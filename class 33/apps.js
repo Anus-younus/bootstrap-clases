5b2215e5caec373f2e7d36866abfeb4ccc1cb3eb
Anus Younus <AnusYounus254@gmail.com>
2024-02-27 16:17:51 +0500

    added class 31

---
+++ class 33/apps.js
@@ -0,0 +1,40 @@
+// var firstRandan = new Date("11 March 2024");
+// var today = new Date();
+
+// // console.log(firstRandan.getTime())
+// // console.log(today.getTime())
+
+// var diff = firstRandan.getTime() - today.getTime();
+
+// var days = diff / (1000 * 60 * 60 * 24);
+// var Month = diff / (1000 * 60 * 60 * 24 * 30);
+// console.log(Math.round(Month) + " Months");
+// console.log(Math.round(days) + "  Days ");
+
+// var date = prompt();
+
+// var a = moment([2007, 0, 29]);
+// var b = moment([2007, 0, 28]);
+
+// console.log(a)
+
+
+// var date = moment().format("");
+// var date = moment().format("D dddd MMMM hh:m A YYYY");
+
+// var a = moment(new Date("26 feb 2024"));
+// var b = moment(new Date());
+// console.log(a.toNow());
+// // console.log(a.fromNow());
+// // console.log(a.diff(b,"hour"));
+
+// var date = moment().format("hh:m A M/ D/ YYYY");
+
+// // console.log(date);
+
+// var date = new Date();
+
+// date.setFullYear(2000);
+
+// date.setMonth(0);
+// console.log(date);
