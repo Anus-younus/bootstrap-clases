f4a3c18a736dd6fed8281cfc07b6ed37d573e551
Anus Younus <AnusYounus254@gmail.com>
2024-02-29 00:37:27 +0500

    modified class 35

--- class 35/app.js
+++ class 35/app.js
@@ -28,9 +28,20 @@ switch (day) {
 }
 */
 
-var str = "5+";
+var str = "5+5-";
 
-var newInput = "+" 
+var newInput = "+";
 
-console.log(newInput.length + 1)
-console.log(str.length - 1);
+// console.log(newInput.length + 1);
+// console.log(str.length - 1);
+
+var last = str[str.length - 1];
+var arr = ["+", "-", "*", "/"];
+
+if (arr.indexOf(last) !== -1 && arr.indexOf(newInput) === -1) {
+  str = str.slice(-1) + last;
+}
+else{
+  str += newInput;
+}
+console.log(str);
