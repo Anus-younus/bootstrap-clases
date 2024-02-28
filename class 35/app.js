e6e24df572a6d823f8abba970de8b7ff8ae646b7
Anus Younus <AnusYounus254@gmail.com>
2024-02-28 23:27:16 +0500

    added class 35

---
+++ class 35/app.js
@@ -0,0 +1,36 @@
+// function submitForm() {
+//   var name = document.getElementById("name");
+//   console.log(name);
+// }
+/*
+function submitForm() {
+    var name = document.getElementById("name");
+    var email = document.getElementById("email");
+    if(name.value.trim() && email.value.trim()){
+    console.log(email.value);
+    console.log(name.value);
+    name.value = ""
+    email.value = ""
+    }else {
+        console.log("please enter input field")
+}
+  }*/
+/*
+let day = "mon";
+switch (day) {
+  case "mon":
+    alert("1");
+    break;
+  case "tue":
+    alert("2");
+  default:
+    alert("3");
+}
+*/
+
+var str = "5+";
+
+var newInput = "+" 
+
+console.log(newInput.length + 1)
+console.log(str.length - 1);
