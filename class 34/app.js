5b74d00b373bd5de4710ac7839498529b0e93176
Anus Younus <AnusYounus254@gmail.com>
2024-02-28 22:40:56 +0500

    added new class 34

---
+++ class 34/app.js
@@ -0,0 +1,77 @@
+/*function abc() {
+  alert("Hello");
+}
+abc();
+*/
+
+/*function x(a) {
+  console.log(a);
+}
+x("Ghous Ahmad");
+x("Anus");
+*/
+/*
+let arr = [];
+
+function myPush(value) {
+  arr[arr.length] = value;
+}
+myPush("Ghous");
+console.log(arr);*/
+/*
+function foo(a, b, c) {
+  console.log(a, b, c);
+}
+foo("Hello", 2323, true);*/
+/*
+function printName(){
+    document.write(name);
+}
+var name = prompt("Enter your name")
+printName(name);*/
+/*
+function printName(){
+    if(userName){
+        document.write(name);
+    }
+}
+let userName = "Anus"
+let name = prompt()
+printName(name)
+*/
+
+// let printName = "";
+// printName();
+/*
+function myFunction(a, b) {
+  return a + b;
+}
+
+let result = myFunction(10, 5);
+console.log(result);
+*/
+/*
+let userName = "Ghous Ahmad";
+let result = userName.toLowerCase();
+console.log(result);
+*/
+/*
+function abc(a,b){
+    var output
+    return output = a * b
+}
+console.log(abc(5,5));
+*/
+/*
+function abc(a, b, c) {
+  alert(a, b, c);
+}
+abc("Hello", "World", "Js");
+*/
+/*
+function foo(){
+    var abc = "Anus Younus"
+    return abc;
+}
+console.log(foo())
+*/
